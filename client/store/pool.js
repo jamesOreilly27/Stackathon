bd9b3944562b879e8d19374b6a0422fa9d0a3111
James O'Reilly <James.Oreilly926@gmail.com>
2017-11-09 12:08:33 -0500

    add allPools functionality to client/store/pool.js

--- client/store/pool.js
+++ client/store/pool.js
@@ -1,15 +1,31 @@
 import axios from 'axios'
 
+const GET_POOLS = 'GET_POOLS'
 const GET_SINGLE_POOL = 'GET_SINGLE_POOL'
 
-const getSinglePool = pool => {
+const gotPools = pools => {
+    return {
+        type: GET_POOLS,
+        payload: pools
+    }
+}
+
+const gotSinglePool = pool => {
     return {
         type: GET_SINGLE_POOL,
         payload: pool
     }
 }
 
-const getOneReivewThunk = pool => dispatch => {
+const fetchPoolThunk = pools => dispatch => {
+    axios.get('/api/pools')
+    .then(res => res.data)
+    .then(pools => {
+        dispatch(getPools(pools))
+    })
+}
+
+const fetchOnePoolThunk = pool => dispatch => {
     axios.get(`/api/pools/${pool.id}`)
     .then(res => res.data)
     .then(pool => {
@@ -18,11 +34,16 @@ const getOneReivewThunk = pool => dispatch => {
     .catch(next)
 }
 
-initialState = {}
+initialState = {
+    pools = [],
+    singlePool: {}
+}
 
 const reducer =  (prevState = initialState, action) => {
     switch (action.type) {
+        case GOT_POOLS:
+            return { ...state, pools: action.payload }
         case GOT_SINGLE_POOL:
-            return action.payload
+            return { ...state, singlePool: action.payload }
     }
 }
