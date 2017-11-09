2a58def80109ca274c69f9d79afdd75c2a113aa3
James O'Reilly <James.Oreilly926@gmail.com>
2017-11-09 11:33:32 -0500

    add getSinglePool action creator in client/store/pool.js

--- client/store/index.js
+++ client/store/index.js
@@ -3,6 +3,7 @@ import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 
 const reducer = combineReducers({  })
+
 const middleware = composeWithDevTools(applyMiddleware(
   thunkMiddleware,
   createLogger({collapsed: true})
--- client/store/pool.js
+++ client/store/pool.js
@@ -1,1 +1,10 @@
 import axios from 'axios'
+
+const GET_SINGLE_POOL = 'GET_SINGLE_POOL'
+
+const getSinglePool = pool => {
+    return {
+        type: GET_SINGLE_POOL,
+        payload: pool
+    }
+}
