e6dd4bafc6280cf3de1a7590b7ebf7b1a0bf9b7d
James O'Reilly <James.Oreilly926@gmail.com>
2017-11-09 15:43:27 -0500

    create odds reducer in client/store/odds.js

--- client/store/odds.js
+++ client/store/odds.js
@@ -8,3 +8,24 @@ const gotOdds = odds => {
         payload: odds
     }
 }
+
+export const fecthNFLOddsThunk = odds => dispatch => {
+    axios.get('/api/odds/nfl')
+    .then(res => res.data)
+    .then(odds => {
+        dispatch(gotOdds(odds))
+    })
+}
+
+const intialState = []
+
+const reducer = (prevState = initialState, action) => {
+    switch (action.type) {
+        case GOT_ODDS:
+            return action.odds
+        default:
+            return prevState
+    }
+}
+
+export default reducer
