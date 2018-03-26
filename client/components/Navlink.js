81a9bab41eb2cb867d61569801f81380caa6dc2c
James O'Reilly <James.Oreilly926@gmail.com>
2018-03-26 17:17:30 -0400

    change all styled given with class names to styled components in client/components/Navlink.js

--- client/components/Navlink.js
+++ client/components/Navlink.js
@@ -1,12 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
+const Container = styled.div`
+  border-radius: .3em;
+
+  &:hover {
+    background-color: rgba(33, 198, 0, 0.74);
+    transition: all .8s;
+  }
+`
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #F3EDED;
+  text-shadow: .01em .01em .05em #F3EDED;
+  font-size: 1.3em;
+  padding: .7em .5em 0;
+  margin: 0;
+`
+
 const Navlink = ({ name }) => (
-  <div className="nav-link-container">
-    <Link to={name}>
+  <Container>
+    <StyledLink to={name}>
       {name}
-    </Link>
-  </div>
+    </StyledLink>
+  </Container>
 )
 
 export default Navlink
