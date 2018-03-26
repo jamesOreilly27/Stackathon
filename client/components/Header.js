9b927e8ebefb17c7f0aea8ae74e200783dbb407a
James O'Reilly <James.Oreilly926@gmail.com>
2018-03-26 16:39:32 -0400

    add styled components 'Wrapper' and 'Container' to Header.js

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,17 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Navlink } from '../components'
 import { Link } from 'react-router-dom'
 
+const Wrapper = styled.header`
+    width: 100vw;
+    position: fixed;
+`
+
+const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    color: #FAFAFA;
+    background-color: #0A0A0A;
+`
+
 const Header = () => (
-        <header>
-            <Link to='/' className="title">  
-                Crypto Book-E
-            </Link>
-            <div className="navbar">
-                <Navlink name="Login" />
-                <Navlink name="Sign-Up" />
-            </div>
-        </header>
+        <Wrapper>
+            <Container>
+                <Link to='/' className="title">  
+                    Crypto Book-E
+                </Link>
+                <div className="navbar">
+                    <Navlink name="Login" />
+                    <Navlink name="Sign-Up" />
+                </div>
+            </Container>
+        </Wrapper>
 )
 
 export default Header
