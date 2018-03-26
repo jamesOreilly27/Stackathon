c3a24e51305bd5786ae7949c0d17001ccd5bea19
James O'Reilly <James.Oreilly926@gmail.com>
2018-03-26 17:47:43 -0400

    change styling done with class names to styled-components in client/components/Sidelink.js

--- client/components/Sidelink.js
+++ client/components/Sidelink.js
@@ -1,12 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-const Sidelink = props => (
-  <div className="sidelink">
-    <Link to={`/pools/${props.path}`}>
-      {props.path}
-    </Link>
-  </div>
+const Container = styled.div`
+  border-radius: .3em;
+
+  &:hover {
+    background-color: rgba(33, 198, 0, 0.74);
+    transition: all .2s;
+  }
+`
+
+const StyledLink = styled(Link)`
+  text-shadow: .01em .01em .05em #F3EDED;
+  text-decoration: none;
+  color: #FAFAFA;
+  font-size: 1.3em;
+`
+
+const Sidelink = ({ path }) => (
+  <Container>
+    <StyledLink to={`/pools/${path}`}>
+      {path}
+    </StyledLink>
+  </Container>
 )
 
 export default Sidelink
