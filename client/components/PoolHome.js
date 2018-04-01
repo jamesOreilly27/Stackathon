80a2c86debdaf64a16e0301e00a6091548556fe1
James O'Reilly <James.Oreilly926@gmail.com>
2018-03-31 22:34:27 -0400

    conditionally render in PoolHome component based on whether or not the user has joined the pool

--- client/components/PoolHome.js
+++ client/components/PoolHome.js
@@ -15,6 +15,8 @@ const Wrapper = styled.div`
 class PoolHome extends Component {
   constructor(props) {
     super(props)
+
+    this.state = { userIsParticipant: false }
   }
 
   isParticipating() {
@@ -33,7 +35,10 @@ class PoolHome extends Component {
     if(this.props.user !== nextProps.user) {
       return this.props.getPoolDetail(this.props.match.params.id)
       .then(response => response.payload)
-      .then(pool => this.props.user.bets.filter(bet => bet.poolId === pool.id))
+      .then(pool => {
+        this.setState({ userIsParticipant: this.isParticipating() })
+        return this.props.user.bets.filter(bet => bet.poolId === pool.id)
+      })
       .then(bets => this.props.userBetsToProps(bets))
       .catch(err => this.props.userBetsToProps(err.message))
     }
@@ -43,8 +48,15 @@ class PoolHome extends Component {
     return (
       <Wrapper>
         <Leaderboard users={this.props.singlePool.users}/>
-        <UsersBets bets={this.props.bets} /> 
-        <MatchTable odds={this.props.odds} poolId={parseInt(this.props.match.params.id)} />
+        {this.state.userIsParticipant
+          ? <div style={{width: '100%'}}>
+              <UsersBets bets={this.props.bets} />
+              <MatchTable odds={this.props.odds} poolId={parseInt(this.props.match.params.id)} />
+            </div>
+          : <div>
+              <button onClick={this.handleClick}> Join Now </button>
+            </div>
+        }
       </Wrapper>
     )
   }
