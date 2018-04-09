9a4d0ef80bb8b61bc6df9b7b15999f8d6dae2eec
James O'Reilly <James.Oreilly926@gmail.com>
2018-04-09 16:24:37 -0400

    add class/instance methods for generating hash and salt of user passwords in server/db/models/user.js

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,5 +1,6 @@
 const db = require('../db');
 const Sequelize = require('sequelize');
+const crypto = require('crypto')
 
 const User = db.define('user', {
   firstName: {
@@ -32,4 +33,30 @@ const User = db.define('user', {
   }
 })
 
-module.exports = User;
+User.prototype.correctPassword = function (candidatePwd) {
+  return User.encryptPassword(candidatePwd, this.salt) === this.password
+}
+
+User.generateSalt = function () {
+  return crypto.randomBytes(16).toString('base64')
+}
+
+User.encryptPassword = function (plainText, salt) {
+  return crypto
+    .createHash('RSA-SHA256')
+    .update(plainText)
+    .update(salt)
+    .digest('hex')
+}
+
+const setSaltAndPassword = user => {
+  if (user.changed('password')) {
+    user.salt = User.generateSalt()
+    user.password = User.encryptPassword(user.password, user.salt)
+  }
+}
+
+User.beforeCreate(setSaltAndPassword)
+User.beforeUpdate(setSaltAndPassword)
+
+module.exports = User
